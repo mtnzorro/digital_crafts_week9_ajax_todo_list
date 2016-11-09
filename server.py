a47448f16a43c5fcd4fb5ecdbb58fc20a3b827db
Jason Campbell <jrcampbell78@gmail.com>
2016-11-09 14:29:23 -0500

    Adds ability to mark out checked tasks and a button to remove them

--- server.py
+++ server.py
@@ -15,12 +15,46 @@ def list_tasks():
 
 @app.route('/add_task', methods=['POST'])
 def add_task():
-    
+
     print request.args
     description = request.form.get('task')
     print description
     result = db.insert('task', description=description)
     return jsonify(result)
 
+@app.route('/mark_task', methods = ['POST'])
+def get_mark():
+    print request.args
+    id = request.form.get('id')
+    done = request.form.get('done')
+    result = db.update('task',id = id, done = done )
+    return jsonify(result)
+
+
+@app.route('/mark_off_task')
+def mark_tasks():
+    results = db.query('select * from task where done = True').dictresult()
+    return jsonify(results)
+
+@app.route('/remove_completed', methods = ['POST'])
+def remove_complete():
+    print request.args
+    id = request.form.get('id')
+    result = db.delete('task', id = id)
+    return jsonify(result);
+
+@app.route('/difficult_remove', methods = ['POST'])
+def difficult_remove():
+    query_array = []
+    query = db.query('select id from task where done = True').dictresult()
+    print query
+    for obj in query:
+        obj = obj.items()
+        print obj
+        for k,v in obj:
+            print v
+            id = v
+            db.delete('task', id = id)
+    return jsonify(query);
 if __name__ == '__main__':
     app.run(debug=True)
--- static/todo.js
+++ static/todo.js
@@ -1,27 +1,63 @@
 $(document).ready(function(){
-  function updateList(callback){
-    $.get('/tasks', callback);
-  }
 
-  updateList(function(stuff){
-    console.log(stuff);
-    stuff.forEach(function(stuffs){
-     $('#task-list').append('<li>' + stuffs.description  + '</li>');
-   });
-  });
+  function updateList(){
+    $.get('/tasks', function(stuff){
+      stuff.forEach(function(stuffs){
+        if (stuffs.done === true){
+          $('#task-list').append('<li + id = "'+ stuffs.id + '"+ class = "done" >' + stuffs.description  +'<input type="checkbox" class="check_box">' + '</li>');
+        }
+        else{
+       $('#task-list').append('<li + id = "'+ stuffs.id + '">' + stuffs.description  +'<input type="checkbox" class="check_box">' + '</li>');
+     }
+     });
+    });
+  }
+  updateList();
 
   $('#form').submit(function(event){
     event.preventDefault();
     $.post('/add_task', $(this).serialize(), function(data){
       $('#task-list').empty();
+      updateList();
     });
 
-  updateList(function(stuff){
-    console.log(stuff);
-    stuff.forEach(function(stuffs){
-     $('#task-list').append('<li + id = "'+ stuffs.id + '">' + stuffs.description  + '</li>');
-   });
   });
+
+  $('#task-list').on('click', 'input', function(){
+    if($(this).prop('checked')){
+      var li_id = $(this).closest('li').attr('id');
+      console.log(li_id);
+      $.post('/mark_task', {"id" : li_id, "done": "True"}, function(){
+        console.log("Checked that thing off");
+      });
+    }
+    $('#task-list').empty();
+    updateList();
+  });
+
+  // $('#remove-completed').click(function(){
+  //   $.get('/tasks', function(stuff){
+  //     stuff.forEach(function(stuffs){
+  //       if (stuffs.done === true){
+  //         console.log("Check for removal: " + stuffs.id);
+  //         $.post('/remove_completed', {"id" : stuffs.id}, function(){
+  //           console.log("Let's give the server their ID: " + stuffs.id);
+  //           $('#task-list').empty();
+  //           updateList();
+  //         });
+  //
+  //     }
+  //
+  //    });
+  //   });
+  //
+  // });
+  $('#remove-completed').click(function(){
+    $.post('/difficult_remove', function(){
+      $('#task-list').empty();
+      updateList();
+      console.log("she's dead jim");
+    });
 
   });
 
