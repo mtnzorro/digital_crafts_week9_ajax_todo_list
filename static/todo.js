d31fc388720e981fb5805f60e5d5f5737550ca39
Jason Campbell <jrcampbell78@gmail.com>
2016-11-09 15:33:53 -0500

    Adds front in remove item functionality

--- static/todo.js
+++ static/todo.js
@@ -3,7 +3,8 @@ $(document).ready(function(){
   function updateList(){
     $.get('/tasks', function(stuff){
       stuff.forEach(function(stuffs){
-        if (stuffs.done === true){          $('#task-list').append('<li + id = "'+ stuffs.id + '"+ class = "done" >' + stuffs.description  +'<input type="checkbox" class="check_box">' + '</li>');
+        if (stuffs.done === true){
+        $('#task-list').append('<li + id = "'+ stuffs.id + '"+ class = "done" >' + stuffs.description  +'<input type="checkbox" class="check_box">' + '</li>');
         }
         else{
        $('#task-list').append('<li + id = "'+ stuffs.id + '">' + stuffs.description  +'<input type="checkbox" class="check_box">' + '</li>');
@@ -34,30 +35,28 @@ $(document).ready(function(){
     updateList();
   });
 
+  $('#remove-completed').click(function(){
+    $.get('/tasks', function(stuff){
+      stuff.forEach(function(stuffs){
+        if (stuffs.done === true){
+          console.log("Check for removal: " + stuffs.id);
+          $.post('/remove_completed', {"id" : stuffs.id}, function(){
+            console.log("Let's give the server their ID: " + stuffs.id);
+          });
+      }
+     });
+    });
+  updateList();
+  });
+
+
   // $('#remove-completed').click(function(){
-  //   $.get('/tasks', function(stuff){
-  //     stuff.forEach(function(stuffs){
-  //       if (stuffs.done === true){
-  //         console.log("Check for removal: " + stuffs.id);
-  //         $.post('/remove_completed', {"id" : stuffs.id}, function(){
-  //           console.log("Let's give the server their ID: " + stuffs.id);
-  //           $('#task-list').empty();
-  //           updateList();
-  //         });
-  //
-  //     }
-  //
-  //    });
+  //   $.post('/difficult_remove', function(){
+  //     $('#task-list').empty();
+  //     updateList();
+  //     console.log("she's dead jim");
   //   });
   //
   // });
-  $('#remove-completed').click(function(){
-    $.post('/difficult_remove', function(){
-      $('#task-list').empty();
-      updateList();
-      console.log("she's dead jim");
-    });
-
-  });
 
 });
